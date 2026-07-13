43f01978cf8e755fdd39bdbcbd7f249625fe7a60
Farook <damareeoomar@gmail.com>
2026-07-13 22:35:31 +0400

    Week 6 Day 1 : Adding POST request to Task Manager project and modifying Github Actions for Week 6 contents

---
+++ Week_6/TaskManagerApi/Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerApi.Controllers;
+
+[ApiController]
+[Route("api/tasks")]
+public class TasksController : ControllerBase
+{
+    private static readonly List<TaskItem> Tasks = new()
+    {
+        new TaskItem { Id = 1, Title = "Learning C#", IsCompleted = true },
+        new TaskItem { Id = 2, Title = "Practicing Programming", IsCompleted = false },
+        new TaskItem { Id = 3, Title = "Learning and failing", IsCompleted = false }
+    };
+
+    [HttpGet]
+    public ActionResult<List<TaskItem>> Get()
+    {
+        return Ok(Tasks);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<TaskItem> Get(int id)
+    {
+        var task = Tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+        {
+            return NotFound($"Task with Id {id} not found.");
+        }
+
+        return Ok(task);
+    }
+
+    [HttpPost]
+    public ActionResult<TaskItem> Post(TaskItem receivedTask)
+    {
+        receivedTask.Id = Tasks.Count + 1;
+        Tasks.Add(receivedTask);
+
+        return Ok(receivedTask);
+        // return CreatedAtAction(nameof(Get), new { id = receivedTask.Id }, receivedTask);
+    }
+}
