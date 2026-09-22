2c9896a23db7a2677639e2da45059cc3727f0d3e
Farook <damareeoomar@gmail.com>
2026-09-23 00:44:19 +0400

    Week 8 Day 2: Configure TaskItem entity mapping

--- Week_9/TaskManagerApi/Models/TaskItem.cs
+++ Week_9/TaskManagerApi/Models/TaskItem.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace TaskManagerApi.Models;
 
 public class TaskItem
 {
     public int Id { get; set; }
+
+    [MaxLength(100)]
     public string Title { get; set; } = "";
     public bool IsCompleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
