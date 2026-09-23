0c7f746f74f78122257d1237d291594d30d7c1fe
Farook <damareeoomar@gmail.com>
2026-09-23 22:30:28 +0400

    Week 9 Day : Configure AppDbContext for SQL Server

--- Week_9/TaskManagerApi/Program.cs
+++ Week_9/TaskManagerApi/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
 using TaskManagerApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("TaskManagerDatabase")
+    ?? throw new InvalidOperationException(
+        "Connection string 'TaskManagerDatabase' was not found.");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<TaskService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
